aad8fb2b9bf8af9e83e5a11d33487d9de8c2eab5
Andrew Brooke <Andrew.Brooke@suffolk.gov.uk>
2019-08-14 11:21:54 +0100

    try

--- eUser-LowerCredentials.ps1
+++ eUser-LowerCredentials.ps1
@@ -1,4 +1,4 @@
-# Preset Variables. Adjust Passfile as required
+# Preset Variables. Adjust Passfile as required.
 $KeyCrypt = (120,80,177,104,51,125,207,56,9,193,73,130,194,179,251,82,35,70,169,109,92,180,55,125,88,209,58,166,75,92,211,116)
 $O365User = 'andrew.brooke@suffolk.gov.uk'
 $O365PassFile = "C:\TEMP\SharePoint-Monitoring\Keys\$O365User.txt"
